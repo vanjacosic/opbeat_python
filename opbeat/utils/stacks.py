de35d60046899d113fc55f552dd62979655edc3a
Ron Cohen <ron@opbeat.com>
2014-11-09 00:25:33 +0100

    Flipped stacktrace order to unify and cleanups.

--- opbeat/utils/stacks.py
+++ opbeat/utils/stacks.py
@@ -156,6 +156,10 @@ def iter_stack_frames(frames=None):
     """
     if not frames:
         frames = inspect.stack()[1:]
+
+    # Opbeat expects deepest last order
+    frames = reversed(frames)
+
     for frame, lineno in ((f[0], f[2]) for f in frames):
         f_locals = getattr(frame, 'f_locals', {})
         if _getitem_from_frame(f_locals, '__traceback_hide__'):
